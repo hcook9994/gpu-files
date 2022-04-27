70a271b7ef4ab2031d33c07751107672ffd90734
hcook9994 <100540026+hcook9994@users.noreply.github.com>
2022-04-27 14:51:45 +1000

    Fixing list as input

--- ganbert-master/ganbert.py
+++ ganbert-master/ganbert.py
@@ -269,7 +269,7 @@ def file_based_convert_examples_to_features(
     features["input_mask"] = create_int_feature(feature.input_mask)
     features["segment_ids"] = create_int_feature(feature.segment_ids)
     features["label_ids"] = create_int_feature([feature.label_id])
-    features["label_mask"] = create_int_feature(int([feature.label_mask]))
+    features["label_mask"] = create_int_feature([int(feature.label_mask)])
     features["is_real_example"] = create_int_feature(
         [int(feature.is_real_example)])
 
