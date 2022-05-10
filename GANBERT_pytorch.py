8ce0190f58d70e5953348c30e1c8c10c506ad647
hcook9994 <100540026+hcook9994@users.noreply.github.com>
2022-05-10 18:02:41 +1000

    Epochs=10, hidden_layers=3

--- GANBERT_pytorch.py
+++ GANBERT_pytorch.py
@@ -73,10 +73,10 @@ batch_size = 64
 #--------------------------------
 # number of hidden layers in the generator, 
 # each of the size of the output space
-num_hidden_layers_g = 1; 
+num_hidden_layers_g = 3; 
 # number of hidden layers in the discriminator, 
 # each of the size of the input space
-num_hidden_layers_d = 1; 
+num_hidden_layers_d = 3; 
 # size of the generator's input noisy vectors
 noise_size = 100
 # dropout to be applied to discriminator's input vectors
@@ -92,7 +92,7 @@ apply_balance = True
 learning_rate_discriminator = 5e-5
 learning_rate_generator = 5e-5
 epsilon = 1e-8
-num_train_epochs = 3
+num_train_epochs = 10
 multi_gpu = True
 # Scheduler
 apply_scheduler = False
